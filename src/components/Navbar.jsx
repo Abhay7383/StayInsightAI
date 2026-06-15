7904420cb223f7dcf2939f13243eb644d9303dab
Abhay Joshi <abhayj2023@gmail.com>
2026-06-15 21:52:02 +0530

    Configure Tailwind CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,4 +8,17 @@ function Navbar() {
   );
 }
 
+// export default Navbar;
+
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+      <h1 className="text-3xl font-bold">
+        StayInsight AI
+      </h1>
+    </nav>
+  );
+}
+
 export default Navbar;
